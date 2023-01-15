407d6fdba7e5b305805abab717bc34a4861b8629
bewu <bewuwy@gmail.com>
2023-01-15 12:29:45 +0100

    initial version

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<!-- create table from leaderboad -->
+<table>
+    <thead>
+        <tr>
+            <th>place</th>
+            <th>name</th>
+            <th>reviews</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each data.leaderboard as record, i}
+            <tr>
+                <td>{i+1}.</td>
+                <td>{record.username}</td>
+                <td>{record.score}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
---
+++ src/routes/+page.ts
@@ -0,0 +1,34 @@
+import PocketBase from 'pocketbase';
+
+
+/** @type {import('./$types').PageLoad} */
+export async function load() { // { params }: any
+
+    const pb = new PocketBase("https://pb-anki-lb.fly.dev/");
+
+    const todayFilter = `updated >= "${new Date().toISOString().split('T')[0]} 00:00:00"`; // "2021-12-31 00:00:00"
+    const records = await pb.collection('today_leaderboard').getFullList(200 /* batch size */, {
+        sort: '-reviews',
+        filter: todayFilter,
+        expand: 'user'
+    });
+
+    const leaderboard = [];
+
+    for (let i = 0; i < records.length; i++) {
+        const record = records[i];
+        const user: any = record.expand.user;
+        const username = user.username;
+
+        const score = record.reviews || 0;
+
+        leaderboard.push({
+            username,
+            score,
+        });
+    }
+
+    return {
+      leaderboard
+    };
+}
