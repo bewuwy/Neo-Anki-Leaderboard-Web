a81ff5eb57a1c9183949bab5e8e29a98c135f676
bewu <bewuwy@gmail.com>
2023-01-17 23:05:46 +0100

    added link to anki addon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,3 +47,8 @@
         {/each}
     </tbody>
 </table>
+
+<footer class="grid">
+    <a href="https://github.com/bewuwy" target="_blank" rel="noopener noreferrer" role="button" class="outline secondary">Made by bewu</a>
+    <a href="https://ankiweb.net/shared/info/1894428367" target="_blank" rel="noopener noreferrer" role="button" class="outline">Anki addon page</a>
+</footer>
