d510650a2edc8c43ded72e89889c64fe5da1192b
bewuwy <bewuwy@gmail.com>
2023-01-22 16:50:13 +0100

    creating user's week and month record

--- src/routes/api/createUser/+server.ts
+++ src/routes/api/createUser/+server.ts
@@ -31,10 +31,24 @@ export async function POST({ request }: any) {
     });
     console.log("user_today_pb", user_today_pb);
 
+    // create in week_leaderboard
+    const user_week_pb = await pb.collection("week_leaderboard").create({
+        "user": user_id,
+        "reviews": 0,
+    });
+
+    // create in month_leaderboard
+    const user_month_pb = await pb.collection("month_leaderboard").create({
+        "user": user_id,
+        "reviews": 0,
+    });
+
     // update user with user_data and user_today
     await pb.collection("users").update(user_id, {
         "user_data": user_data_pb.id,
         "user_today": user_today_pb.id,
+        "user_week": user_week_pb.id,
+        "user_month": user_month_pb.id,
     });
 
     return json({"success": true});
