d29effe48c24dfdded001347c0dc50fa2d7bd941
bewuwy <bewuwy@gmail.com>
2023-01-30 16:16:52 +0100

    updated footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,10 @@
-<footer class="grid">
-    <a href="https://github.com/bewuwy" target="_blank" rel="noopener noreferrer" role="button" class="outline secondary">Made by bewu</a>
-    <a href="https://ankiweb.net/shared/info/1894428367" target="_blank" rel="noopener noreferrer" role="button" class="outline">Anki addon page</a>
+<footer>
+    <section class="grid">
+        <a href="https://github.com/bewuwy/Neo-Anki-Leaderboard" target="_blank" rel="noopener noreferrer" role='button' class="outline secondary">Source code</a>
+        <a href="https://ankiweb.net/shared/info/1894428367" target="_blank" rel="noopener noreferrer" role="button" class="outline">Anki addon page</a>
+    </section>
+    
+    <div dir="rtl">
+        Made by <a href="https://bewu.dev" target="_blank" rel="noopener noreferrer" class="outline">bewu</a>
+    </div>
 </footer>
