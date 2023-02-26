aaccc6a3ba537d4d5beeb22e92ab16a1fe603b38
bewu <bewuwy@gmail.com>
2023-02-26 21:33:57 +0100

    removed x-checkmark from user info

--- src/lib/UserInfo.svelte
+++ src/lib/UserInfo.svelte
@@ -69,11 +69,11 @@
             <title>User has verified their email address</title>
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
-        {:else}
+        <!-- {:else}
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
             <title>User hasn't verified their email address yet</title>
             <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
+        </svg> -->
         {/if}
     </h1>
     <h3>account created: {createdDate}</h3>
