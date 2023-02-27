45ec7a49e60b6d5900cee9e6021822f4632e9cbf
bewu <bewuwy@gmail.com>
2023-02-27 23:25:55 +0100

    added input to show first user match

--- src/routes/custom/+page.svelte
+++ src/routes/custom/+page.svelte
@@ -12,35 +12,45 @@
     }
 
     let userSearch: string = "";
+    let userFirstMatch: string = "";
     let members: User[] = [];
 
     const pb = new Pocketbase(PUBLIC_PB_URL);
 
-    function handleEnter(event: KeyboardEvent) {
+    function handleSearch(event: KeyboardEvent) {
+        if (userSearch.length == 0) {
+            userFirstMatch = "";
+        }
+        else {
+            pb.collection("users").getFirstListItem(`username~"${userSearch}"`).then((res: any) => {
+                userFirstMatch = res.username;
+            }).catch((err: any) => {
+                userFirstMatch = "";
+            });
+        }
+
         if (event.key === "Enter") {
             // console.log(userSearch);
             getUserID(userSearch);
-
-            userSearch = "";
         }
     }
 
     function handleSearchButton() {
         getUserID(userSearch);
-
-        userSearch = "";
     }
 
     function getUserID(username: string) {
         pb.collection("users").getFirstListItem(`username~"${username}"`).then((res: any) => {
 
-            if (members.includes(res.id)) {
+            if (members.some((member) => { return member.id == res.id })) {
                 toast.error("User already added");
-                return;
             }
-
-            members = [...members, { id: res.id, username: res.username }];
-            toast.success("User added");
+            else {
+                members = [...members, { id: res.id, username: res.username }];
+                toast.success(`${res.username} added`);
+            }
+            
+            userSearch = "";
 
         }).catch((err: any) => {
             toast.error("User not found");
@@ -53,6 +63,16 @@
 
         members = members.filter((member) => member.id != id);
         toast.success("User removed");
+    }
+
+    function handleRemoveKey(event: KeyboardEvent) {
+        if (event.key === "Enter") {
+            let id = (event.target as HTMLElement).id;
+            // console.log(id);
+
+            members = members.filter((member) => member.id != id);
+            toast.success("User removed");
+        }
     }
 
     function handleCreate() {
@@ -76,6 +96,10 @@
 </script>
 
 <style>
+    #members > li {
+        cursor: pointer;
+    }
+
     #members > li:hover {
         /* stroke through */
         text-decoration: line-through;
@@ -84,8 +108,17 @@
     #searchParent {
         display: grid;
         grid-template-columns: 4fr 1fr;
-        grid-template-rows: 1fr;
+        grid-template-rows: 1fr 1fr;
         grid-column-gap: 1rem;
+    }
+
+    #searchParent > #searchInput {
+        grid-column: 1 / 3;
+        grid-row: 1 / 2;
+    }
+    #searchParent > button {
+        grid-column: 2 / 3;
+        grid-row: 2 / 3;
     }
 </style>
 
@@ -104,8 +137,9 @@
 <h4>Search for users</h4>
 
 <div id="searchParent">
-    <input placeholder="Search for an user..." type="text" on:keydown={handleEnter} bind:value={userSearch}>
+    <input placeholder="Search for an user..." id="searchInput" type="text" on:keyup={handleSearch} bind:value={userSearch}>
     <button on:click={handleSearchButton}>Add</button>
+    <input type="text" readonly value={userFirstMatch || "No users found"}>
 </div>
 
 <!-- <ul role="listbox">
@@ -116,11 +150,12 @@
 
 <h4>Added members</h4>
 {#if members.length == 0}
-    <p>None users added yet</p>
+    <p>No users added yet</p>
 {/if}
 <ul id="members">
     {#each members as member}
-        <li id={member.id} on:click={handleRemove}>{member.username}</li>
+        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+        <li id={member.id} tabindex="0" on:click={handleRemove} on:keydown={handleRemoveKey}>{member.username}</li>
     {/each}
 </ul>
 
